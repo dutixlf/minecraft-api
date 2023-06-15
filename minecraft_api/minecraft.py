440fdfacb5a8edaeec134f28a8b6f613aec39eec
dutixlf <zahatema2016@ya.ru>
2023-06-15 22:07:25 +0500

    Fixed library + setup.py

--- minecraft_api/minecraft.py
+++ minecraft_api/minecraft.py
@@ -7,15 +7,15 @@ class Connect:
         if Path(f'{session}.session').is_file():
             with open(f'{session}.session', 'r+') as f:
                 data = str(f.read()).split('\n')
-                self.mcr = MCRcon(data[0], data[1], data[2])
-                print(self.mcr)
+                self.mcr = MCRcon(host=data[0], password=data[1], port=int(data[2]))
         else:
             with open(f'{session}.session', 'w+') as f:
                 host = str(input('Enter hostname: '))
                 port = int(input('Enter port: '))
                 password = str(input('Enter password: '))
-                f.write(f'{host}\n{port}\n{password}')
-                self.mcr = MCRcon(host, port, password)
+                f.write(f'{host}\n{password}\n{port}')
+                self.mcr = MCRcon(host=host, password=password, port=port)
+        self.mcr.connect()
     def sendCommand(self, cmd=str):
         h = self.mcr.command(cmd)
         return h
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='minecraft_api',
+    version='1.0.0',
+    packages=['mcrcon'],
+    url='https://github.com/dutixlf/minecraft-api/',
+    license='',
+    author='dutixlf',
+    author_email='',
+    description=''
+)
