900651192a243590d3149c4497d0d12643f666de
dutixlf <zahatema2016@ya.ru>
2023-06-15 23:38:47 +0500

    some fixes

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from setuptools import setup
 setup(
     name='minecraft_api',
     version='1.0.0',
-    packages=['mcrcon'],
+    install_requires=['mcrcon'],
     url='https://github.com/dutixlf/minecraft-api/',
-    license='',
     author='dutixlf',
-    author_email='',
-    description=''
 )
